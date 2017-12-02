6f1f47eaaf9658d98e94422fbd5175cf55e2f7d5
Ben Morgan <neembi@gmail.com>
2017-12-02 15:12:27 +0100

    Completed challenge for day 1

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,9 @@
-extern crate num;
-
-pub fn inverse_captcha<I: IntoIterator>(it: I) -> I::Item
-where I::Item: num::Integer + num::Zero + Copy
-{
-    let mut sum = num::zero();
-    let mut it = it.into_iter();
-
-    let first;
-    let mut last;
-    match it.next() {
-        Some(i) => {
-            first = i;
-            last = i;
-            },
-        None => {return sum; }
-    }
-
-    while let Some(i) = it.next() {
-        if last == i {
-            sum = sum + last
-        }
-        last = i;
-    }
-
-    if first == last {
-        sum + first
-    } else {
-        sum
-    }
+// Calculate the captcha result for the printer for day 1.
+// This works for part one (n=1) and part two (n=vec.len()/2).
+pub fn inverse_captcha<'a, I>(it: I, n: usize) -> u32
+where I: Iterator<Item = &'a u32> + std::clone::Clone {
+    let y = it.clone().cycle().skip(n);
+    it.zip(y).fold(0, |acc, (x,y)| if x == y { acc + x } else { acc })
 }
 
 #[cfg(test)]
@@ -37,15 +13,17 @@ mod tests {
     #[test]
     fn inverse_captcha_correct_sum() {
         let tests = vec![
-            (vec![1,1,2,2], 3),
-            (vec![1,1,1,1], 4),
-            (vec![1,2,3,4], 0),
-            (vec![1,1,1,2], 2),
-            (vec![9,1,2,1,2,1,2,9], 9),
+            (vec![1,1,2,2], 3, 0),
+            (vec![1,2,1,2], 0, 6),
+            (vec![1,1,1,1], 4, 4),
+            (vec![1,2,3,4], 0, 0),
+            (vec![1,1,1,2], 2, 2),
+            (vec![9,1,2,1,2,1,2,9], 9, 6),
         ];
 
         for t in tests {
-            assert_eq!(inverse_captcha(t.0), t.1);
+            assert_eq!(inverse_captcha(t.0.iter(), 1), t.1);
+            assert_eq!(inverse_captcha(t.0.iter(), t.0.len()/2), t.2);
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,40 @@
 use std::io;
-use std::io::prelude::*;
+
+extern crate clap;
+use clap::{App, Arg};
 
 extern crate aoc;
 
-#[allow(dead_code)]
-fn read_stdin_split_on_space<T: std::str::FromStr>() -> Vec<T> {
-    let mut v = Vec::new();
-    let stdin = io::stdin();
-    for line in stdin.lock().lines() {
-        for field in line.unwrap().split(|c: char| c.is_whitespace()) {
-            if field.is_empty() {
-                continue;
-            }
-            match field.parse::<T>() {
-                Ok(x) => v.push(x),
-                _ => continue,
-            }
-        }
-    }
-    v
-}
-
 fn read_serial_number(s: &str) -> Vec<u32> {
-    s.chars().map(|c| c.to_digit(10).unwrap()).collect()
+    s.chars().filter(|c| c.is_digit(10)).map(|c| c.to_digit(10).unwrap()).collect()
 }
 
-fn main() {
+fn day1() {
+    println!("Day 1: the inverse captcha.");
+
+    println!(" -> reading from stdin");
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
     let v = read_serial_number(input.as_str());
-    println!("{}", aoc::inverse_captcha(v));
+
+    println!(" -> (a) {}", aoc::inverse_captcha(v.iter(), 1));
+    println!(" -> (b) {}", aoc::inverse_captcha(v.iter(), v.len() / 2));
+}
+
+fn main() {
+    let matches = App::new("Advent of Code 2017")
+        .version("0.2.0")
+        .author("Ben Morgan <neembi@gmail.com")
+        .arg(Arg::with_name("DAY")
+             .required(true)
+             .help("Which day to run")
+             .index(1))
+        .get_matches();
+
+    let day = matches.value_of("DAY").unwrap();
+    let day = day.parse::<u32>().unwrap_or(0);
+    match day {
+        1 => day1(),
+        _ => println!("Unknown day, try something between 1 and 2."),
+    }
 }
