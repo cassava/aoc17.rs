7b86893ff80e381b7edca9d245904472f63310c4
Ben Morgan <neembi@gmail.com>
2017-12-16 22:42:44 +0100

    Adding documentation for day 7

--- src/bin/07_tree.rs
+++ src/bin/07_tree.rs
@@ -1,3 +1,100 @@
+/*!
+# Day 7: Recursive Circus
+
+Wandering further through the circuits of the computer, you come upon a tower of programs that have
+gotten themselves into a bit of trouble. A recursive algorithm has gotten out of hand, and now
+they're balanced precariously in a large tower.
+
+One program at the bottom supports the entire tower. It's holding a large disc, and on the disc are
+balanced several more sub-towers. At the bottom of these sub-towers, standing on the bottom disc,
+are other programs, each holding their own disc, and so on. At the very tops of these
+sub-sub-sub-...-towers, many programs stand simply keeping the disc below them balanced but with no
+disc of their own.
+
+You offer to help, but first you need to understand the structure of these towers. You ask each
+program to yell out their name, their weight, and (if they're holding a disc) the names of the
+programs immediately above them balancing on that disc. You write this information down (your
+puzzle input). Unfortunately, in their panic, they don't do this in an orderly fashion; by the time
+you're done, you're not sure which program gave which information.
+
+For example, if your list is the following:
+
+```text
+pbga (66)
+xhth (57)
+ebii (61)
+havc (66)
+ktlj (57)
+fwft (72) -> ktlj, cntj, xhth
+qoyq (66)
+padx (45) -> pbga, havc, qoyq
+tknk (41) -> ugml, padx, fwft
+jptl (61)
+ugml (68) -> gyxo, ebii, jptl
+gyxo (61)
+cntj (57)
+```
+
+...then you would be able to recreate the structure of the towers that looks like this:
+
+```text
+                gyxo
+              /     
+         ugml - ebii
+       /      \     
+      |         jptl
+      |        
+      |         pbga
+     /        /
+tknk --- padx - havc
+     \        \
+      |         qoyq
+      |             
+      |         ktlj
+       \      /     
+         fwft - cntj
+              \     
+                xhth
+```
+
+In this example, tknk is at the bottom of the tower (the bottom program), and is holding up ugml,
+padx, and fwft. Those programs are, in turn, holding up other programs; in this example, none of
+those programs are holding up any other programs, and are all the tops of their own towers. (The
+actual tower balancing in front of you is much larger.)
+
+Before you're ready to help them, you need to make sure your information is correct.
+What is the name of the bottom program?
+
+## Part Two
+
+The programs explain the situation: they can't get down. Rather, they could get down, if they
+weren't expending all of their energy trying to keep the tower balanced. Apparently, one program
+has the wrong weight, and until it's fixed, they're stuck here.
+
+For any program holding a disc, each program standing on that disc forms a sub-tower. Each of those
+sub-towers are supposed to be the same weight, or the disc itself isn't balanced. The weight of
+a tower is the sum of the weights of the programs in that tower.
+
+In the example above, this means that for ugml's disc to be balanced, gyxo, ebii, and jptl must all
+have the same weight, and they do: 61.
+
+However, for tknk to be balanced, each of the programs standing on its disc and all programs above
+it must each match. This means that the following sums must all be the same:
+
+```text
+    ugml + (gyxo + ebii + jptl) = 68 + (61 + 61 + 61) = 251
+    padx + (pbga + havc + qoyq) = 45 + (66 + 66 + 66) = 243
+    fwft + (ktlj + cntj + xhth) = 72 + (57 + 57 + 57) = 243
+```
+
+As you can see, tknk's disc is unbalanced: ugml's stack is heavier than the other two. Even though
+the nodes above ugml are balanced, ugml itself is too heavy: it needs to be 8 units lighter for its
+stack to weigh 243 and keep the towers balanced. If this change were made, its weight would be 60.
+
+Given that exactly one program is the wrong weight, what would its weight need to be to balance the
+entire tower?
+*/
+
 extern crate aoc;
 extern crate regex;
 #[macro_use] extern crate lazy_static;
@@ -42,10 +139,12 @@ impl Node {
         node
     }
 
-    // Unsafe
-    //
-    // This function uses unsafe and might leak memory or perform a double-free.
-    // It has not been extensively tested.
+    /// Builds a `Node` from an iterator of `NodeDefinition`.
+    ///
+    /// # Safety
+    ///
+    /// This function uses unsafe and might leak memory or perform a double-free.
+    /// It has not been extensively tested.
     pub fn from_iter<'a, I>(it: I) -> Result<Box<Self>, ParseError>
     where I: Iterator<Item = NodeDefinition> {
         let mut nodes: HashMap<String, *mut Node> = HashMap::new();
@@ -119,6 +218,14 @@ impl Node {
 
     pub fn name(&self) -> &str { self.name.as_str() }
 
+    /// Returns the weight of the node.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let node = Node::with_weight("a", 1);
+    /// assert_eq!(node.weight(), 1);
+    /// ```
     pub fn weight(&self) -> u32 { self.weight }
 
     pub fn get_balance(&self) -> u32 {
@@ -149,7 +256,7 @@ impl Node {
             .or_else(|| self.get_unbalanced_child())
     }
 
-    // Returns the unbalanced child, assuming it exists.
+    /// Returns the unbalanced child, assuming it exists.
     fn get_unbalanced_child(&self) -> Option<BalanceOp> {
         let mut counts = HashMap::new();
         self.children.iter().for_each(|x| {
@@ -225,18 +332,18 @@ impl FromStr for NodeDefinition {
 
 #[derive(Debug)]
 pub enum ParseError {
-    // Error due to some node referring to a child that does not exist.
+    /// Error due to some node referring to a child that does not exist.
     RefNotExist{
         from: String,
         to: String,
     },
 
-    // Error due to a malformed node definition.
+    /// Error due to a malformed node definition.
     Malformed{
         line: String,
     },
 
-    // Error due to tree not simply connected, i.e., has multiple roots.
+    /// Error due to tree not simply connected, i.e., has multiple roots.
     NotConnected{
         roots: Vec<String>,
     }
