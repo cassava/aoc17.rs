88372b93d97dac4e4d8bc4481ccaf186a8fe281a
Ben Morgan <neembi@gmail.com>
2018-02-03 01:38:23 +0100

    Initial unrefactored completion of day 14

--- src/bin/14_defrag.rs
+++ src/bin/14_defrag.rs
@@ -92,32 +92,78 @@ fn main() {
 
     let disk = Disk::from(input.to_str());
     println!(":: Answer 1 is {}", disk.used());
+    println!(":: Answer 2 is {}", disk.regions());
 }
 
-pub struct Bit {
-    set: bool,
+#[derive(Clone, Copy, Debug)]
+pub struct Bit(isize);
+
+impl Default for Bit {
+    fn default() -> Self {
+        Bit(0)
+    }
 }
 
 impl Bit {
     pub const UNSET_CHAR: char = '.';
     pub const SET_CHAR: char = '#';
 
-    fn new(set: bool) -> Self {
-        Self {
-            set: set,
+    pub fn new(set: bool) -> Self {
+        Bit(if set { -1 } else { 0 })
+    }
+
+    pub fn from_hex(n: u8) -> [Bit; 4] {
+        assert!(n.leading_zeros() >= 4);
+        let mut array = [Bit::default(); 4];
+        for i in 0..4 {
+            array[i] = Bit::new(n & (1 << (3 - i)) != 0);
         }
+        array
+    }
+
+    pub fn unset(&mut self) {
+        self.0 = 0;
+    }
+
+    pub fn set(&mut self) {
+        self.0 = -1;
+    }
+
+    pub fn set_region(&mut self, region: usize) {
+        assert!(region <= isize::max_value() as usize);
+        self.0 = region as isize;
+    }
+
+    /// Returns the region of a set bit.
+    ///
+    /// All unset bits are automatically in region 0.
+    pub fn region(&self) -> Option<usize> {
+        if self.0 < 0 {
+            None
+        } else {
+            Some(self.0 as usize)
+        }
+    }
+
+    pub fn is_set(&self) -> bool {
+        self.0 != 0
     }
 }
 
 impl fmt::Display for Bit {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        f.write_char(if self.set { Bit::SET_CHAR } else { Bit::UNSET_CHAR })
+        f.write_char(match self.region() {
+            Some(0) => Bit::UNSET_CHAR,
+            Some(n) => ('0' as usize + n % 10) as u8 as char,
+            None => Bit::SET_CHAR,
+        })
     }
 }
 
+#[derive(Clone, Debug)]
 pub struct Disk {
     /// Contains a vector of 0-1 strings.
-    space: Vec<Vec<bool>>
+    space: Vec<Vec<Bit>>,
 }
 
 impl Disk {
@@ -126,44 +172,100 @@ impl Disk {
     pub fn from(s: &str) -> Self {
         // Generate 128 modifications of input.
         Disk {
-            space: (0..Self::NUM_BLOCKS)
+            space: (0..Disk::NUM_BLOCKS)
                 .map(|i| {
                     let hash = knot::hash(format!("{}-{}", s, i).as_str());
+                    assert_eq!(hash.len(), 32);
                     hash.chars()
-                        .map(|c| c.to_digit(16).unwrap() as u8)
-                        // needs to be split into 8 bools
+                        .flat_map(|c| Bit::from_hex(c.to_digit(16).unwrap() as u8).to_vec())
                         .collect()
                 })
-                .collect()
+                .collect(),
         }
     }
 
     pub fn used(&self) -> usize {
         self.space.iter().fold(0, |acc, blk| {
-            acc + blk.iter().fold(0, |acc, b| acc + if *b { 1 } else { 0 })
+            acc + blk.iter().map(|b| b.is_set() as usize).sum::<usize>()
         })
     }
 
-    pub fn regions(&self) -> usize {
-        // Not sure how I want to go about this.
-        let mut blocks: Vec<_> = format!("{}", self).lines().map(|x| String::from(x)).collect();
-        for li in 0..Disk::NUM_BLOCKS {
-            
-        }
+    pub fn bit(&self, (row, col): (usize, usize)) -> &Bit {
+        &self.space[row][col]
+    }
 
-        0
+    pub fn bit_mut(&mut self, (row, col): (usize, usize)) -> &mut Bit {
+        &mut self.space[row][col]
+    }
+
+    pub fn regions(&self) -> usize {
+        let mut disk = self.clone();
+        let mut count = 0;
+        for row in 0..Disk::NUM_BLOCKS {
+            for col in 0..Disk::NUM_BLOCKS {
+                let idx = (row, col);
+                if disk.bit(idx).region().is_none() {
+                    count += 1;
+                    disk.set_region_from(idx, count);
+                }
+                assert!(disk.bit(idx).region().is_some());
+            }
+        }
+        count
+    }
+
+    fn set_region_from(&mut self, idx: (usize, usize), region: usize) {
+        let mut stack = vec![idx];
+        loop {
+            match stack.pop() {
+                None => break,
+                Some(idx) => {
+                    let b = self.bit_mut(idx);
+                    if b.region().is_none() {
+                        b.set_region(region);
+                        stack.append(&mut neighbours(Disk::NUM_BLOCKS, idx));
+                    }
+                }
+            }
+        }
     }
 }
 
 impl fmt::Display for Disk {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         for line in self.space.iter() {
-            let mut s = String::with_capacity(Disk::NUM_BLOCKS);
             line.iter().for_each(|b| write!(f, "{}", b).unwrap());
             f.write_char('\n')?;
         }
         Ok(())
     }
+}
+
+fn neighbours_wrapping(block_size: usize, (row, col): (usize, usize)) -> Vec<(usize, usize)> {
+    vec![
+        ((row - 1) % block_size, col),
+        ((row + 1) % block_size, col),
+        (row, (col - 1) % block_size),
+        (row, (col + 1) % block_size),
+    ]
+}
+
+fn neighbours(block_size: usize, (row, col): (usize, usize)) -> Vec<(usize, usize)> {
+    let max_idx = block_size - 1;
+    let mut vec = Vec::new();
+    if row > 0 {
+        vec.push((row - 1, col));
+    }
+    if row < max_idx {
+        vec.push((row + 1, col));
+    }
+    if col > 0 {
+        vec.push((row, col - 1));
+    }
+    if col < max_idx {
+        vec.push((row, col + 1));
+    }
+    vec
 }
 
 #[cfg(test)]
@@ -172,9 +274,16 @@ mod tests {
 
     #[test]
     fn test_used_blocks() {
-        let test = ("flqrgnkx", 8108);
-        assert_eq!(Disk::from(test.0).used(), 8108);
-        assert_eq!(Disk::regions(), 1242);
+        let disk = Disk::from("flqrgnkx");
+        assert_eq!(disk.used(), 8108);
+    }
+
+    #[test]
+    fn test_regions() {
+        let mut disk = Disk::from("flqrgnkx");
+        let regions = disk.regions();
+        println!("\n{}", disk);
+        assert_eq!(regions, 1242);
     }
 }
 
