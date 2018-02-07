e5d9e86c21b0f15de14ea86a9e949d015b786703
Ben Morgan <neembi@gmail.com>
2018-02-08 00:01:05 +0100

    WIP: Day 17 completed but refactoring in progress
    Not sure if it is possible to do part one without allocating
    anything on the heap.
    
    Next steps:
    
    1. Now that I know where the value was set, try to find out
       how many times it is written, and which write I'm interested
       in.

--- src/bin/17_vortex.rs
+++ src/bin/17_vortex.rs
@@ -57,6 +57,23 @@ value written `(2017)`, you can short-circuit the spinlock. In this example, tha
 would be 638.
 
 What is the value after 2017 in your completed circular buffer?
+
+## Part Two
+
+The spinlock does not short-circuit. Instead, it gets more angry. At least, you
+assume that's what happened; it's spinning significantly faster than it was
+a moment ago.
+
+You have good news and bad news.
+
+The good news is that you have improved calculations for how to stop the
+spinlock. They indicate that you actually need to identify the value after 0 in
+the current state of the circular buffer.
+
+The bad news is that while you were determining this, the spinlock has just
+finished inserting its fifty millionth value (50000000).
+
+What is the value after 0 the moment 50000000 is inserted?
 */
 
 extern crate aoc;
@@ -65,8 +82,155 @@ fn main() {
     let mut input = aoc::ProgramInput::new(PUZZLE, INPUT);
     println!("Day 17: {}", PUZZLE);
 
-    println!(":: Answer 1 is {}", "");
-    println!(":: Answer 2 is {}", "");
+    let steps: usize = input.to_str().parse().unwrap();
+
+    println!(":: Answer 1 is {}", whirlwind_next(steps, 2017));
+    println!(
+        ":: Answer 2 is {}",
+        whirlwind(steps, 50_000_000, 1).watch_value.unwrap()
+    );
+}
+
+#[derive(Debug, PartialEq, PartialOrd)]
+struct WhirlMatch {
+    pub last_position: usize,
+    pub watch_set_at: usize,
+    pub watch_value: Option<usize>,
+}
+
+fn whirlwind(steps: usize, until: usize, watch: usize) -> WhirlMatch {
+    let mut len = 1;
+    let mut pos = 0;
+    let mut val = if watch == 0 { Some(0) } else { None };
+
+    // The value at 0 is always the same, we can skip the watching.
+    if watch == 0 {
+        for _ in 1..(until + 1) {
+            pos = ((pos + steps) % len) + 1;
+            len += 1;
+        }
+        return WhirlMatch {
+            last_position: pos,
+            watch_set_at: 0,
+            watch_value: Some(0),
+        };
+    }
+
+    // If the watch index is higher than 1, we need to assume that it could
+    // have been pushed there at a later time. So we need to keep track
+    // how many times the value sitting at i was pushed to the left, that
+
+    let mut slider = watch;
+    for i in 1..(until + 1) {
+        pos = ((pos + steps) % len) + 1;
+        len += 1;
+
+        // Update the slider
+        if pos > watch {
+            ;
+        } else if pos == watch {
+            val = Some(i);
+            slider = pos;
+        } else if pos < slider {
+            slider -= 1;
+        } else if slider < pos {
+            slider += pos - slider;
+        }
+        assert!(slider <= watch);
+    }
+
+    // Do the whole thing again, now watch the value of slider though.
+    len = 1;
+    pos = 0;
+    val = if slider == 0 { Some(0) } else { None };
+    for i in 1..(until + 1) {
+        pos = ((pos + steps) % len) + 1;
+        len += 1;
+
+        // Update the value
+        if pos == slider {
+            val = Some(i);
+        }
+    }
+
+    WhirlMatch {
+        last_position: pos,
+        watch_set_at: slider,
+        watch_value: val,
+    }
+}
+
+fn whirlwind_next(steps: usize, until: usize) -> usize {
+    // First, find out what the last index is, then use that.
+    let WhirlMatch {
+        last_position: pos, ..
+    } = whirlwind(steps, until, 0);
+
+    let watch = pos + 1;
+    if watch % (until + 1) == 0 {
+        0
+    } else {
+        whirlwind(steps, until, watch).watch_value.unwrap()
+    }
+}
+
+#[test]
+fn test_whirlwind_zerostep() {
+    for until in 0..100 {
+        for watch in 0..until {
+            assert_eq!(
+                whirlwind(0, until, watch),
+                WhirlMatch {
+                    last_position: until,
+                    watch_set_at: watch,
+                    watch_value: Some(watch),
+                }
+            );
+        }
+    }
+}
+
+#[test]
+fn test_whirlwind_next_zerostep() {
+    for until in 0..1000 {
+        assert_eq!(whirlwind_next(0, until), 0);
+    }
+}
+
+#[test]
+fn test_whirlwind_basics() {
+    let tests: Vec<(usize, usize, Vec<usize>, usize)> = vec![
+        (3, 0, vec![0], 0),
+        (3, 1, vec![0, 1], 1),
+        (3, 2, vec![0, 2, 1], 1),
+        (3, 3, vec![0, 2, 3, 1], 2),
+        (3, 4, vec![0, 2, 4, 3, 1], 2),
+        (3, 5, vec![0, 5, 2, 4, 3, 1], 1),
+        (3, 6, vec![0, 5, 2, 4, 3, 6, 1], 5),
+        (3, 7, vec![0, 5, 7, 2, 4, 3, 6, 1], 2),
+        (3, 8, vec![0, 5, 7, 2, 4, 3, 8, 6, 1], 6),
+        (3, 9, vec![0, 9, 5, 7, 2, 4, 3, 8, 6, 1], 1),
+    ];
+    for (steps, until, results, pos) in tests {
+        let n = results.len();
+        assert_eq!(until + 1, n);
+        for index in 0..n {
+            let mat = whirlwind(steps, until, index);
+            assert_eq!(mat.last_position, pos);
+            assert_eq!(
+                mat.watch_value,
+                Some(results[index]),
+                "watch index {} with test {:?} => {:?}",
+                index,
+                results,
+                mat,
+            );
+        }
+        assert_eq!(
+            whirlwind_next(steps, until),
+            results[(pos + 1) % (until + 1)]
+        );
+    }
 }
 
 const PUZZLE: &'static str = "Spinlock";
